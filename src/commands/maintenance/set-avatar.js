fd131677effd1f4a0f21afbefc82edf5af308c85
Joaquim <joaquimmy@yahoo.com>
2020-09-02 14:31:18 -0300

    add set-avatar command

---
+++ src/commands/maintenance/set-avatar.js
@@ -0,0 +1,31 @@
+const { Command, Permission } = require('@ponatech/bot');
+const got = require('got');
+const { logger } = require('../../modules');
+
+module.exports = new Command({
+  name: 'set-avatar',
+  description: "Changes Hanabi-sama's avatar.",
+  permission: Permission.BOT_ADMIN,
+  category: 'maintenance',
+  requiredArgs: ['image url'],
+  async run(bot, message, meta) {
+    const imageUrl = meta.args[0];
+    let res;
+    try {
+      res = await got(imageUrl, { responseType: 'buffer' });
+    } catch {
+      this.send("That's not a valid image...");
+      return;
+    }
+    if (!res.headers['content-type']?.startsWith('image')) {
+      this.send("Looks like that's not an image...");
+      return;
+    }
+    logger.log('Settings avatar to:', res.body);
+    await bot.user.setAvatar(res.body);
+    this.send('Am I cute? owo~');
+    this.send(bot.user.avatarURL())
+  },
+});
+// potato 🍟potato
+// potato🍠potato
