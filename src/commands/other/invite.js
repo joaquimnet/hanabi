92f184a0ba2a718c8989513bf77b8a05e9244dfe
Joaquim <joaquimmy@yahoo.com>
2020-09-09 12:17:27 -0300

    change appearence of invite command

--- src/commands/other/invite.js
+++ src/commands/other/invite.js
@@ -6,8 +6,21 @@ module.exports = new Command({
   aliases: ['inviteme'],
   category: 'other',
   hidden: false,
-  async run() {
+  async run(bot, message, meta) {
     const invite = `https://discordapp.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&permissions=392256&scope=bot`;
-    this.send("I'm so happy you want to invite me c:", invite);
+    meta.respond({
+      embed: {
+        title: 'Invite Hanabi!',
+        description: "I'm so happy you want to invite me c:",
+        fields: [
+          {
+            name: 'Invite Link',
+            value: invite,
+          },
+        ],
+        thumbnail: { url: bot.user.avatarURL({ size: 1024 }) },
+        color: 0x00ff00,
+      },
+    });
   },
 });
