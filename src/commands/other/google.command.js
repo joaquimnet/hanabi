592bbd43d6b4c6bc5426b6ff58621b63ef19b4b6
Joaquim Neto <joaquimmy@gmail.com>
2021-06-19 22:13:46 -0300

    add no-sandbox arg to puppeteer

--- src/commands/other/google.command.js
+++ src/commands/other/google.command.js
@@ -39,6 +39,7 @@ async function screenshot(url) {
   const browser = await puppeteer.launch({
     headless: true,
     defaultViewport: { width: 1920, height: 900 },
+    args: ['--no-sandbox'],
   });
   const page = await browser.newPage();
   await page.goto(url);
