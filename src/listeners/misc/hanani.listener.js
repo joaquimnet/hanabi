5b4bac223b32c2cf07937442c741d2ceea3121fb
Joaquim Neto <joaquimmy@gmail.com>
2021-06-20 18:11:23 -0300

    add max length on hanani listener

--- src/listeners/misc/hanani.listener.js
+++ src/listeners/misc/hanani.listener.js
@@ -5,6 +5,7 @@ module.exports = new Listener({
   category: 'misc',
   cooldown: 180000,
   priority: 0,
+  maxMessageLength: 100,
   run(bot, message, meta) {
     meta.respond("My name is **Hanabi**, not Hanani.... :sweat: And it's __**Hanabi-sama**__ to *you*.");
     return true;
