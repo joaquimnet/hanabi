01c7050190ccb4c6e2437c1bf9a5562698960b42
Joaquim Neto <joaquimmy@gmail.com>
2021-06-20 22:17:05 -0300

    add a few config messages

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 require('tinv')();
+const { TextHelpers } = require('sensum');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -25,6 +26,23 @@ const config = {
   messages: {
     COOLDOWN: 'Please wait **{0}** before using the {1} command again.',
     USAGE: "You're missing the **{0}** argument! \nUsage: {1}",
+    COMMAND_FEEDBACK_SERVER_ONLY:
+      'The {0} command is unavailable via private message. Please run it in a server.',
+    COMMAND_FEEDBACK_DM_ONLY:
+      'The {0} command is only available via private message. Please run it in the DMs.',
+    COMMAND_FEEDBACK_MISSING_PERMISSION: TextHelpers.lines(
+      'You do not have permission to use this command.',
+      `Your permission level is {0} ({1})`,
+      `This command requires level {2} ({3})`,
+    ),
+    COMMAND_FEEDBACK_MISSING_ARGS_SINGULAR: TextHelpers.lines(
+      `Looks like you have a problem with your args.`,
+      '{0}',
+    ),
+    COMMAND_FEEDBACK_MISSING_ARGS_PLURAL: TextHelpers.lines(
+      `Looks like you have a few problems with your args.`,
+      '{0}',
+    ),
   },
 
   // Help Category Emotes
