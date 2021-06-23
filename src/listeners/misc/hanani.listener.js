a9f603a4e78d36b3d3f17877d69f94850b708d2f
Joaquim Neto <joaquimmy@gmail.com>
2021-06-23 12:18:12 -0300

    update cooldown on janani listener

--- src/listeners/misc/hanani.listener.js
+++ src/listeners/misc/hanani.listener.js
@@ -3,9 +3,10 @@ const { Listener } = require('sensum');
 module.exports = new Listener({
   words: ['hanani'],
   category: 'misc',
-  cooldown: 180000,
+  cooldown: 10,
   priority: 0,
   maxMessageLength: 100,
+  globalCooldown: 120,
   run(bot, message, meta) {
     meta.respond(
       "My name is **Hanabi**, not Hanani.... :sweat: And it's __**Hanabi-sama**__ to *you*.",
