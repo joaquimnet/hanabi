a66640335e0c50a080cfdf8beaf4a2f8d04a0411
xbabyblu <xlilblu666@gmail.com>
2021-06-29 10:35:09 -0400

    updated listener global cooldown

--- src/listeners/Hanabi!/age.listener.js
+++ src/listeners/Hanabi!/age.listener.js
@@ -6,6 +6,7 @@ module.exports = new Listener({
   words: ['how', 'old', 'are', 'you', 'hanabi'],
   category: 'hanabi',
   cooldown: 600,
+  globalCooldown: 1800,
   priority: 0,
   // as of todays date: she is 291 days old :D 06.20.2021
   run(bot, message, meta) {
