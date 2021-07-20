18e4de8136557dc144c951012dcc0f9a80bf9072
Joaquim Neto <joaquimmy@gmail.com>
2021-07-19 22:21:01 -0300

    fix vote event

--- src/voting/vote.event.js
+++ src/voting/vote.event.js
@@ -2,10 +2,19 @@ const { EventHandler } = require('sensum');
 
 const Vote = require('./vote.model.v1');
 
+const VOTE_REWARD_AMOUNT = 500;
+
 module.exports = new EventHandler({
   name: 'vote',
   async run(bot, vote) {
-    await Vote.addVote(vote.user, vote.type, vote.isWeekend);
+    // TODO: fix duplicate key error
+    await Vote.addVote(vote.user, vote.type, vote.isWeekend).catch(() => {});
+
+    const profile = await bot.getProfile(vote.user).catch(() => {});
+    if (profile) {
+      await profile.giveMoney(bot, VOTE_REWARD_AMOUNT).catch(() => {});
+    }
+
     const user = await bot.users.fetch(vote.user);
     await bot.alerts.sendSuccess({
       type: 'Hanabi got a vote!',
