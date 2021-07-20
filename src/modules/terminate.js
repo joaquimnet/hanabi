8fad7cbb273b891a4ff21ee69c3bbd8b61d86a5f
Joaquim Neto <joaquimmy@gmail.com>
2021-07-20 13:04:09 -0300

    send alert when she crashes

--- src/modules/terminate.js
+++ src/modules/terminate.js
@@ -6,30 +6,73 @@ const terminate = (type, bot) => {
     process.exit(code);
   };
 
-  // TODO: Send alert!
-
   switch (type) {
     case 'exception':
       return (err) => {
         logger.error('An uncaught exception ocurred. Terminating...');
         logger.error(err);
-        bot?.destroy();
-        mongoose.disconnect();
-        exit(1);
+        bot.alerts
+          .sendError({
+            title: 'Fatal error! Hanabi is restarting...',
+            message:
+              '```' +
+              JSON.stringify({ err, env: process.env.NODE_ENV }, null, 2) +
+              '```',
+            thumbnail:
+              'https://img.freepik.com/free-vector/warning-sign-black-background_97458-374.jpg?size=626&ext=jpg',
+          })
+          .then(() => {
+            bot?.destroy();
+            mongoose.disconnect();
+            exit(1);
+          })
+          .catch(() => {
+            exit(1);
+          });
       };
     case 'rejection':
       return (reason, promise) => {
-        logger.error('A promise rejected without a catch. Terminating...');
+        logger.error('A promise rejected without a catch.');
         logger.error(reason);
         logger.error(promise);
-        bot?.destroy();
-        mongoose.disconnect();
-        exit(1);
+        bot.alerts
+          .sendError({
+            title: 'Fatal error! Hanabi is restarting...',
+            message:
+              '```' +
+              JSON.stringify(
+                { reason, promise, env: process.env.NODE_ENV },
+                null,
+                2,
+              ) +
+              '```',
+            thumbnail:
+              'https://img.freepik.com/free-vector/warning-sign-black-background_97458-374.jpg?size=626&ext=jpg',
+          })
+          .then(() => {
+            bot?.destroy();
+            mongoose.disconnect();
+            exit(1);
+          })
+          .catch(() => {
+            exit(1);
+          });
       };
     case 'db_failure':
       return (err) => {
         logger.error('Failed to connect to database. Terminating...');
         logger.error(err);
+        bot.alerts
+          .sendError({
+            title: 'Could not connect to database...',
+            message:
+              '```' +
+              JSON.stringify({ err, env: process.env.NODE_ENV }, null, 2) +
+              '```',
+            thumbnail:
+              'https://img.freepik.com/free-vector/warning-sign-black-background_97458-374.jpg?size=626&ext=jpg',
+          })
+          .catch(() => {});
         exit(1);
       };
     default:
