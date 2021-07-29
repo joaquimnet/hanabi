61cd588cd0e12baab7c852147373bfbb747d200d
Joaquim Neto <joaquimmy@gmail.com>
2021-07-29 20:11:22 -0300

    send alerts to dev server when an error occurs

--- src/events/error.event.js
+++ src/events/error.event.js
@@ -4,5 +4,18 @@ module.exports = new EventHandler({
   name: 'error',
   run(bot, err) {
     bot.logger.error(err);
+    bot.alerts
+      .sendError({
+        title: 'Error 🔴😡🥵',
+        message:
+          '```' +
+          JSON.stringify({ err, env: process.env.NODE_ENV }, null, 2) +
+          '```',
+        thumbnail:
+          'https://img.freepik.com/free-vector/warning-sign-black-background_97458-374.jpg?size=626&ext=jpg',
+      })
+      .catch(() => {
+        bot.logger.error('Could not send error alert to Dev Server.');
+      });
   },
 });
--- src/events/warn.event.js
+++ src/events/warn.event.js
@@ -4,5 +4,18 @@ module.exports = new EventHandler({
   name: 'warn',
   run(bot, err) {
     bot.logger.error(err);
+    bot.alerts
+      .sendWarn({
+        title: 'Warning ⚠',
+        message:
+          '```' +
+          JSON.stringify({ err, env: process.env.NODE_ENV }, null, 2) +
+          '```',
+        thumbnail:
+          'https://img.freepik.com/free-vector/warning-sign-black-background_97458-374.jpg?size=626&ext=jpg',
+      })
+      .catch(() => {
+        bot.logger.error('Could not send error alert to Dev Server.');
+      });
   },
 });
