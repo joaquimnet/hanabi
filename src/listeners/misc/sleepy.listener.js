8d02647bd13ef399c931978b42c32f7380d964ff
Joaquim Neto <joaquimmy@gmail.com>
2021-08-06 19:57:14 -0300

    add grammar to sleepy listener

--- src/listeners/misc/sleepy.listener.js
+++ src/listeners/misc/sleepy.listener.js
@@ -7,7 +7,7 @@ module.exports = new Listener({
   globalCooldown: 6000,
   priority: 0,
   run(bot, message, meta) {
-    meta.respond('Take a nap then, silly.:sleeping::orange_heart:');
+    this.send(bot.brain.think('Sleepy'));
     return true;
   },
 });
