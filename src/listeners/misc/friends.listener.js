f9af632858c5cb686e9088c3b5004970997778ec
Joaquim Neto <joaquimmy@gmail.com>
2021-09-11 02:37:20 -0300

    feat(friends.listener): make her think about it

--- src/listeners/misc/friends.listener.js
+++ src/listeners/misc/friends.listener.js
@@ -1,13 +1,15 @@
 const { Listener } = require('sensum');
 
 module.exports = new Listener({
-  words: 'i wish i had friends',
+  words: ['{me}', 'wish', '(had|have)', 'friends'],
   category: 'misc',
   cooldown: 1800,
   globalCooldown: 1800,
   priority: 0,
   run(bot, message, meta) {
-    meta.respond("I'm sure so many people would love to be friends with you. Making friends can be hard and stressful, but I do believe in you.");
+    const msg = bot.brain.think('Friends');
+    this.send(msg);
+    // meta.respond("I'm sure so many people would love to be friends with you. Making friends can be hard and stressful, but I do believe in you.");
     return true;
   },
 });
