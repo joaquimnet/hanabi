9dd7ff117a65ce33667d90c88375e28ba3fd7fb3
Joaquim Neto <joaquimmy@gmail.com>
2021-09-10 22:15:17 -0300

    format(*): run formatter

--- src/listeners/misc/watertimer.listener.js
+++ src/listeners/misc/watertimer.listener.js
@@ -10,12 +10,13 @@ module.exports = new Listener({
     message.channel.startTyping().catch(() => {});
     await bot.wait(3000);
     const msg = bot.brain.think('Water Timer', meta);
-    meta.respond(
-      // "Hey! I am popping in real quick to remind you to try and correct your posture and drink some water if you haven't in a while! Maybe grab a snack if you haven't eaten in a while as well!",
-      msg
-    )
-    .then(() => message.channel.stopTyping())
-    .catch(() =>{});
+    meta
+      .respond(
+        // "Hey! I am popping in real quick to remind you to try and correct your posture and drink some water if you haven't in a while! Maybe grab a snack if you haven't eaten in a while as well!",
+        msg,
+      )
+      .then(() => message.channel.stopTyping())
+      .catch(() => {});
     return true;
   },
 });
